7a75de80d342fc883e6f240e91900c3fb21f1497
Gabriel Gomes <gabrielgms0@hotmail.com>
2025-08-19 14:38:02 -0300

    fix: atualização do controller com utilização dos modelos de DTO de response

--- E-SportsAPP/Controllers/PlayerController.cs
+++ E-SportsAPP/Controllers/PlayerController.cs
@@ -21,14 +21,14 @@ namespace E_SportsAPP.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllPlayers()
+        public async Task<ActionResult<IEnumerable<PlayerResponseDTO>>> GetAllPlayers()
         {
             var players = await _playerRepository.GetAllPlayersAsync();
-            return Ok(players);
+            return Ok(_mapper.Map<IEnumerable<PlayerResponseDTO>>(players));
         }
 
         [HttpGet("GetByName/{name}")]
-        public async Task<IActionResult> GetPlayerByName(string name)
+        public async Task<ActionResult<PlayerResponseDTO>> GetPlayerByName(string name)
         {
             var player = await _playerRepository.GetPlayerByNameAsync(name);
             if (player == null)
@@ -36,29 +36,31 @@ namespace E_SportsAPP.Controllers
                 return NotFound();
             }
 
-             return Ok(player);
+             return Ok(_mapper.Map<PlayerResponseDTO>(player));
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetPlayerById(int id)
+        public async Task<ActionResult<PlayerDetailDTO>> GetPlayerById(int id)
         {
             var player = await _playerRepository.GetPlayerByIdAsync(id);
             if (player == null)
             {
                 return NotFound();
             }
-            return Ok(player);
+            return Ok(_mapper.Map<PlayerDetailDTO>(player));
         }
 
         [HttpPost]
-        public async Task<ActionResult<Player>> AddPlayer(Player player)
+        public async Task<ActionResult<PlayerResponseDTO>> AddPlayer([FromBody] Player player)
         {
             if (player == null)
             {
                 return BadRequest("Player não pode ser nulo.");
             }
             await _playerRepository.AddPlayerAsync(player);
-            return CreatedAtAction(nameof(GetPlayerById), new { id = player.Id }, player);
+
+            var playerDto = _mapper.Map<PlayerResponseDTO>(player);
+            return CreatedAtAction(nameof(GetPlayerById), new { id = player.Id }, playerDto);
         }
 
         [HttpPut("{id}")]
