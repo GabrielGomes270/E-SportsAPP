e58917be6cf4d48592d7f84bcb074b957ddb7b86
Gabriel Gomes <gabrielgms0@hotmail.com>
2025-08-19 14:08:33 -0300

    feat: adição de DTOs de response no JSON

---
+++ E-SportsAPP/DTOs/GearDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace E_SportsAPP.DTOs
+{
+    public class GearDetailDTO
+    {
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int PlayerId { get; set; }
+    }
+}
