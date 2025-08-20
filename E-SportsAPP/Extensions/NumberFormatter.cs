8323cddff9a840fe5c488b7ae10fe3f53f7ef99a
Gabriel Gomes <gabrielgms0@hotmail.com>
2025-08-20 20:27:55 -0300

    feat: Adiciona formatação para números no response

---
+++ E-SportsAPP/Extensions/NumberFormatter.cs
@@ -0,0 +1,32 @@
+﻿namespace E_SportsAPP.Extensions
+{
+    public static class NumberFormatter
+    {
+
+        public static string ToReadableFormat(this int number)
+        {
+            if (number >= 1_000_000)
+                return (number / 1_000_000D).ToString("0.#") + "M";
+            if (number >= 1_000)
+                return (number / 1_000D).ToString("0.#") + "K";
+            return number.ToString();
+        }
+        public static string ToReadableFormat(this long number)
+        {
+            if (number >= 1_000_000)
+                return (number / 1_000_000D).ToString("0.#") + "M";
+            if (number >= 1_000)
+                return (number / 1_000D).ToString("0.#") + "K";
+            return number.ToString();
+        }
+
+        public static string ToReadableFormat(this decimal number)
+        {
+            if (number >= 1_000_000)
+                return (number / 1_000_000M).ToString("0.#") + "M";
+            if (number >= 1_000)
+                return (number / 1_000M).ToString("0.#") + "K";
+            return number.ToString("0.##");
+        }
+    }
+}
--- E-SportsAPP/Migrations/20250820200929_ImageTable.Designer.cs
+++ E-SportsAPP/Migrations/20250820200929_ImageTable.Designer.cs
@@ -3,6 +3,7 @@ using E_SportsAPP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_SportsAPP.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250820200929_ImageTable")]
+    partial class ImageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
