8b6d9c956093da26046218554aeab4a5fa215a8c
Gabriel Gomes <gabrielgms0@hotmail.com>
2025-08-21 10:34:00 -0300

    feat: Adiciona endpoint PATCH para marcar/desmarcar um jogador Highlighted

--- E-SportsAPP/Repositories/IPlayerRepository.cs
+++ E-SportsAPP/Repositories/IPlayerRepository.cs
@@ -8,6 +8,7 @@ namespace E_SportsAPP.Repositories
         Task<IEnumerable<Player>> GetHighlightedPlayersByAsync();
         Task<Player?> GetPlayerByIdAsync(int id);
         Task <List<Player>> GetPlayerByNameAsync(string name);
+        Task <IEnumerable<Player>> GetPlayersByRoleAsync(string role);
         Task AddPlayerAsync(Player player);
         Task UpdatePlayerAsync(int id, Player player);
         Task DeletePlayerAsync(int id);
